fd27e1c82560b2ae07dd8915559c58afb77707e8
Master <internalmike@gmail.com>
2026-06-20 16:37:13 +0300

    potion creation

--- Data Files/Scripts/A1_Alchemy/alchemy.lua
+++ Data Files/Scripts/A1_Alchemy/alchemy.lua
@@ -6,6 +6,377 @@ local input = require('openmw.input')
 local types = require('openmw.types')
 local self = require('openmw.self')
 local core = require('openmw.core')
+local ambient = require('openmw.ambient')
+
+local function potion5(broken, cheap, standard, qualitative, exclusive)
+    return {
+        type = 5,
+        broken = broken,
+        cheap = cheap,
+        standard = standard,
+        qualitative = qualitative,
+        exclusive = exclusive,
+    }
+end
+
+local function potion1(standard, difficulty)
+    return {
+        type = 1,
+        standard = standard,
+        difficulty = difficulty,
+    }
+end
+
+local potions = {
+    ['restorehealth'] = potion5(
+        'p_restore_health_b', 'p_restore_health_c', 'p_restore_health_s',
+        'p_restore_health_q', 'p_restore_health_e'
+    ),
+    ['restorefatigue'] = potion5(
+        'p_restore_fatigue_b', 'p_restore_fatigue_c', 'p_restore_fatigue_s',
+        'p_restore_fatigue_q', 'p_restore_fatigue_e'
+    ),
+    ['restorespellpoints'] = potion5(
+        'p_restore_magicka_b', 'p_restore_magicka_c', 'p_restore_magicka_s',
+        'p_restore_magicka_q', 'p_restore_magicka_e'
+    ),
+    ['waterwalking'] = potion5(
+        'p_water_walking_b', 'p_water_walking_c', 'p_water_walking_s',
+        'p_water_walking_q', 'p_water_walking_e'
+    ),
+    ['waterbreathing'] = potion5(
+        'p_water_breathing_b', 'p_water_breathing_c', 'p_water_breathing_s',
+        'p_water_breathing_q', 'p_water_breathing_e'
+    ),
+    ['levitate'] = potion5(
+        'p_levitation_b', 'p_levitation_c', 'p_levitation_s',
+        'p_levitation_q', 'p_levitation_e'
+    ),
+    ['telekinesis'] = potion5(
+        'p_telekinesis_b', 'p_telekinesis_c', 'p_telekinesis_s',
+        'p_telekinesis_q', 'p_telekinesis_e'
+    ),
+    ['cureblightdisease'] = potion1('p_cure_blight_s', 200),
+    ['curecommondisease'] = potion1('p_cure_common_s', 400),
+    ['detectkey'] = potion5(
+        'p_detect_key_b', 'p_detect_key_c', 'p_detect_key_s', 'p_detect_key_q', 'p_detect_key_e'
+    ),
+    ['curepoison'] = potion1('p_cure_poison_s', 400),
+    ['feather'] = potion5('p_feather_b', 'p_feather_c', 'p_feather_s', 'p_feather_q', 'p_feather_e'),
+    ['resistfire'] = potion5(
+        'p_fire_resistance_b', 'p_fire_resistance_c', 'p_fire resistance_s',
+        'p_fire_resistance_q', 'p_fire_resistance_e'
+    ),
+    ['restoreattribute:personality'] = potion5(
+        'p_restore_personality_b', 'p_restore_personality_c', 'p_restore_personality_s',
+        'p_restore_personality_q', 'p_restore_personality_e'
+    ),
+    ['fortifyspellpoints'] = potion5(
+        'p_fortify_magicka_b', 'p_fortify_magicka_c', 'p_fortify_magicka_s',
+        'p_fortify_magicka_q', 'p_fortify_magicka_e'
+    ),
+    ['nighteye'] = potion5(
+        'p_night-eye_b', 'p_night-eye_c', 'p_night-eye_s', 'p_night-eye_q', 'p_night-eye_e'
+    ),
+    ['detectanimal'] = potion5(
+        'p_detect_creatures_b', 'p_detect_creatures_c', 'p_detect_creatures_s',
+        'p_detect_creatures_q', 'p_detect_creatures_e'
+    ),
+    ['cureparalyzation'] = potion1('p_cure_paralyzation_s', 400),
+    ['detectenchantment'] = potion5(
+        'p_detect_enchantment_b', 'p_detect_enchantment_c', 'p_detect_enchantment_s',
+        'p_detect_enchantment_q', 'p_detect_enchantment_e'
+    ),
+    ['dispel'] = potion5('p_dispel_b', 'p_dispel_c', 'p_dispel_s', 'p_dispel_q', 'p_dispel_e'),
+    ['fireshield'] = potion5(
+        'p_fire_shield_b', 'p_fire_shield_c', 'p_fire_shield_s', 'p_fire_shield_q', 'p_fire_shield_e'
+    ),
+    ['fortifyattribute:agility'] = potion5(
+        'p_fortify_agility_b', 'p_fortify_agility_c', 'p_fortify_agility_s',
+        'p_fortify_agility_q', 'p_fortify_agility_e'
+    ),
+    ['fortifyattackbonus'] = potion5(
+        'p_fortify_attack_b', 'p_fortify_attack_c', 'p_fortify_attack_s',
+        'p_fortify_attack_q', 'p_fortify_attack_e'
+    ),
+    ['fortifyattribute:endurance'] = potion5(
+        'p_fortify_endurance_b', 'p_fortify_endurance_c', 'p_fortify_endurance_s',
+        'p_fortify_endurance_q', 'p_fortify_endurance_e'
+    ),
+    ['fortifyfatigue'] = potion5(
+        'p_fortify_fatigue_b', 'p_fortify_fatigue_c', 'p_fortify_fatigue_s',
+        'p_fortify_fatigue_q', 'p_fortify_fatigue_e'
+    ),
+    ['fortifyhealth'] = potion5(
+        'p_fortify_health_b', 'p_fortify_health_c', 'p_fortify_health_s',
+        'p_fortify_health_q', 'p_fortify_health_e'
+    ),
+    ['fortifyattribute:intellegence'] = potion5(
+        'p_fortify_intelligence_b', 'p_fortify_intelligence_c', 'p_fortify_intelligence_s',
+        'p_fortify_intelligence_q', 'p_fortify_intelligence_e'
+    ),
+    ['fortifyattribute:luck'] = potion5(
+        'p_fortify_luck_b', 'p_fortify_luck_c', 'p_fortify_luck_s', 'p_fortify_luck_q', 'p_fortify_luck_e'
+    ),
+    ['fortifyattribute:personality'] = potion5(
+        'p_fortify_personality_b', 'p_fortify_personality_c', 'p_fortify_personality_s',
+        'p_fortify_personality_q', 'p_fortify_personality_e'
+    ),
+    ['fortifyattribute:speed'] = potion5(
+        'p_fortify_speed_b', 'p_fortify_speed_c', 'p_fortify_speed_s',
+        'p_fortify_speed_q', 'p_fortify_speed_e'
+    ),
+    ['fortifyattribute:strength'] = potion5(
+        'p_fortify_strength_b', 'p_fortify_strength_c', 'p_fortify_strength_s',
+        'p_fortify_strength_q', 'p_fortify_strength_e'
+    ),
+    ['fortifyattribute:willpower'] = potion5(
+        'p_fortify_willpower_b', 'p_fortify_willpower_c', 'p_fortify_willpower_s',
+        'p_fortify_willpower_q', 'p_fortify_willpower_e'
+    ),
+    ['resistfrost'] = potion5(
+        'p_frost_resistance_b', 'p_frost_resistance_c', 'p_frost_resistance_s',
+        'p_frost_resistance_q', 'p_frost_resistance_e'
+    ),
+    ['frostshield'] = potion5(
+        'p_frost_shield_b', 'p_frost_shield_c', 'p_frost_shield_s', 'p_frost_shield_q', 'p_frost_shield_e'
+    ),
+    ['invisibility'] = potion5(
+        'p_invisibility_b', 'p_invisibility_c', 'p_invisibility_s', 'p_invisibility_q', 'p_invisibility_e'
+    ),
+    ['lightningshield'] = potion5(
+        'p_lightning shield_b', 'p_lightning shield_c', 'p_lightning shield_s',
+        'p_lightning shield_q', 'p_lightning shield_e'
+    ),
+    ['resistmagicka'] = potion5(
+        'p_magicka_resistance_b', 'p_magicka_resistance_c', 'p_magicka_resistance_s',
+        'p_magicka_resistance_q', 'p_magicka_resistance_e'
+    ),
+    ['resistpoison'] = potion5(
+        'p_poison_resistance_b', 'p_poison_resistance_c', 'p_poison_resistance_s',
+        'p_poison_resistance_q', 'p_poison_resistance_e'
+    ),
+    ['reflect'] = potion5(
+        'p_reflection_b', 'p_reflection_c', 'p_reflection_s', 'p_reflection_q', 'p_reflection_e'
+    ),
+    ['restoreattribute:agility'] = potion5(
+        'p_restore_agility_b', 'p_restore_agility_c', 'p_restore_agility_s',
+        'p_restore_agility_q', 'p_restore_agility_e'
+    ),
+    ['restoreattribute:endurance'] = potion5(
+        'p_restore_endurance_b', 'p_restore_endurance_c', 'p_restore_endurance_s',
+        'p_restore_endurance_q', 'p_restore_endurance_e'
+    ),
+    ['restoreattribute:intellegence'] = potion5(
+        'p_restore_intelligence_b', 'p_restore_intelligence_c', 'p_restore_intelligence_s',
+        'p_restore_intelligence_q', 'p_restore_intelligence_e'
+    ),
+    ['restoreattribute:luck'] = potion5(
+        'p_restore_luck_b', 'p_restore_luck_c', 'p_restore_luck_s', 'p_restore_luck_q', 'p_restore_luck_e'
+    ),
+    ['restoreattribute:speed'] = potion5(
+        'p_restore_speed_b', 'p_restore_speed_c', 'p_restore_speed_s', 'p_restore_speed_q', 'p_restore_speed_e'
+    ),
+    ['restoreattribute:strength'] = potion5(
+        'p_restore_strength_b', 'p_restore_strength_c', 'p_restore_strength_s',
+        'p_restore_strength_q', 'p_restore_strength_e'
+    ),
+    ['restoreattribute:willpower'] = potion5(
+        'p_restore_willpower_b', 'p_restore_willpower_c', 'p_restore_willpower_s',
+        'p_restore_willpower_q', 'p_restore_willpower_e'
+    ),
+    ['spellabsorption'] = potion5(
+        'p_spell_absorption_b', 'p_spell_absorption_c', 'p_spell_absorption_s',
+        'p_spell_absorption_q', 'p_spell_absorption_e'
+    ),
+    ['swiftswim'] = potion5(
+        'p_swift_swim_b', 'p_swift_swim_c', 'p_swift_swim_s', 'p_swift_swim_q', 'p_swift_swim_e'
+    ),
+    ['mark'] = potion1('p_mark_s', 400),
+    ['recall'] = potion1('p_recall_s', 100),
+    ['resistcommondisease'] = potion5(
+        'p_disease_resistance_b', 'p_disease_resistance_c', 'p_disease_resistance_s',
+        'p_disease_resistance_q', 'p_disease_resistance_e'
+    ),
+    ['shield'] = potion5('p_silence_b', 'p_silence_c', 'p_silence_s', 'p_silence_q', 'p_silence_e'),
+    ['jump'] = potion5('p_jump_b', 'p_jump_c', 'p_jump_s', 'p_jump_q', 'p_jump_e'),
+    ['slowfall'] = potion5('p_slowfall_b', 'p_slowfall_c', 'p_slowfall_s', 'p_slowfall_q', 'p_slowfall_e'),
+    ['chameleon'] = potion5(
+        'p_chameleon_b', 'p_chameleon_c', 'p_chameleon_s', 'p_chameleon_q', 'p_chameleon_e'
+    ),
+    ['light'] = potion5('p_light_b', 'p_light_c', 'p_light_s', 'p_light_q', 'p_light_e'),
+    ['sanctuary'] = potion5(
+        'p_sanctuary_b', 'p_sanctuary_c', 'p_sanctuary_s', 'p_sanctuary_q', 'p_sanctuary_e'
+    ),
+    ['almsiviintervention'] = potion1('p_almsivi_intervention_s', 100),
+    ['resistshock'] = potion5(
+        'p_shock_resistance_b', 'p_shock_resistance_c', 'p_shock_resistance_s',
+        'p_shock_resistance_q', 'p_shock_resistance_e'
+    ),
+    ['resistblightdisease'] = potion5(
+        'p_blight_resistance_b', 'p_blight_resistance_c', 'p_blight_resistance_s',
+        'p_blight_resistance_q', 'p_blight_resistance_e'
+    ),
+    ['resistnormalweapons'] = potion5('p_burden_b', 'p_burden_c', 'p_burden_s', 'p_burden_q', 'p_burden_e'),
+    ['resistparalysis'] = potion5(
+        'p_paralyze_b', 'p_paralyze_c', 'p_paralyze_s', 'p_paralyze_q', 'p_paralyze_e'
+    ),
+    ['summoncenturionsphere'] = potion5(
+        'p_summon_centurion_sphere_b', 'p_summon_centurion_sphere_c', 'p_summon_centurion_sphere_s',
+        'p_summon_centurion_sphere_q', 'p_summon_centurion_sphere_e'
+    ),
+}
+
+local function potion(effect)
+    local id = effect.effect.id
+    if id == 'restoreattribute' or id == 'fortifyattribute' then
+        id = id .. ':' .. effect.affectedAttribute
+    end
+    return potions[id]
+end
+
+local function alchemy()
+    local alchemy = types.NPC.stats.skills.alchemy(self.object).modified
+    local luck = types.Actor.stats.attributes.luck(self.object).modified
+    local int = types.Actor.stats.attributes.intelligence(self.object).modified
+    return alchemy + 0.1 * (int + luck)
+end
+
+local function mortarValue(quality)
+    if quality < 0.75 then
+        return 50
+    elseif quality < 1.1 then
+        return 68
+    elseif quality < 1.35 then
+        return 86
+    else
+        return 104
+    end
+end
+
+local function retortValue(quality)
+    if quality < 0.25 then
+        return 50
+    elseif quality < 0.75 then
+        return 62
+    elseif quality < 1.1 then
+        return 75
+    elseif quality < 1.35 then
+        return 87
+    else
+        return 100
+    end
+end
+
+local function alembicValue(quality)
+    if quality < 0.1 then
+        return 41
+    elseif quality < 0.75 then
+        return 59
+    elseif quality < 1.1 then
+        return 77
+    elseif quality < 1.35 then
+        return 95
+    else
+        return 113
+    end
+end
+
+local function calcinatorValue(quality)
+    if quality < 0.25 then
+        return 0
+    elseif quality < 0.75 then
+        return 3
+    elseif quality < 1.1 then
+        return 6
+    elseif quality < 1.35 then
+        return 10
+    else
+        return 20
+    end
+end
+
+local function makePotion5(mortar, retort, alembic, calcinator)
+    local alch = alchemy()
+    if alch < retort then
+        retort = alch
+    end
+    alch = alch + calcinator
+    if alch < mortar then
+        mortar = alch
+    end
+    if alch < alembic then
+        alembic = alch
+    end
+    alembic = alembic - 9
+    if alembic > mortar then
+        alembic = mortar
+    end
+    if alembic < 15 then
+        return 0
+    end
+    if math.random() * 100 >= retort then
+        return 0
+    end
+    alch = alembic + math.random() * (mortar + 1 - alembic)
+    if alch < 33 then
+        return 1
+    elseif alch < 51 then
+        return 2
+    elseif alch < 69 then
+        return 3
+    elseif alch < 87 then
+        return 4
+    else
+        return 5
+    end
+end
+
+local function makePotion1(mortar, retort, alembic, calcinator, difficulty)
+    local alch = alchemy()
+    if alch < retort then
+        retort = alch
+    end
+    alch = alch + calcinator
+    if alch < mortar then
+        mortar = alch
+    end
+    if alch < alembic then
+        alembic = alch
+    end
+    alembic = alembic - 9
+    if alembic > mortar then
+        alembic = mortar
+    end
+    if alembic < 15 then
+        return 0
+    end
+    if math.random() * 100 >= retort then
+        return 0
+    end
+    alch = alembic + math.random() * (mortar + 1 - alembic)
+    if alch < 33 then
+        alch = 16
+    elseif alch < 51 then
+        alch = 8
+    elseif alch < 69 then
+        alch = 4
+    elseif alch < 87 then
+        alch = 2
+    else
+        alch = 1
+    end
+    alch = difficulty / alch
+    local res = 0
+    if alch >= 100 then
+        alch = alch - 100
+        res = 1
+    end
+    if math.random() * 100 < alch then
+        res = res + 1
+    end
+    return res
+end
 
 local function commonEffects(ingr1, ingr2)
     if ingr1.icon == ingr2.icon then
@@ -27,37 +398,37 @@ local function commonEffects(ingr1, ingr2)
 end
 
 local negativeEffects = {
-    ['Burden'] = true,
-    ['FireDamage'] = true,
-    ['ShockDamage'] = true,
-    ['FrostDamage'] = true,
-    ['DrainAttribute'] = true,
-    ['DrainHealth'] = true,
-    ['DrainSpellpoints'] = true,
-    ['DrainFatigue'] = true,
-    ['DrainSkill'] = true,
-    ['DamageAttribute'] = true,
-    ['DamageHealth'] = true,
-    ['DamageMagicka'] = true,
-    ['DamageFatigue'] = true,
-    ['DamageSkill'] = true,
-    ['Poison'] = true,
-    ['WeaknessToFire'] = true,
-    ['WeaknessToFrost'] = true,
-    ['WeaknessToShock'] = true,
-    ['WeaknessToMagicka'] = true,
-    ['WeaknessToCommonDisease'] = true,
-    ['WeaknessToBlightDisease'] = true,
-    ['WeaknessToCorprusDisease'] = true,
-    ['WeaknessToPoison'] = true,
-    ['WeaknessToNormalWeapons'] = true,
-    ['DisintegrateWeapon'] = true,
-    ['DisintegrateArmor'] = true,
-    ['Paralyze'] = true,
-    ['Silence'] = true,
-    ['Blind'] = true,
-    ['Sound'] = true,
-    ['StuntedMagicka'] = true,
+    ['burden'] = true,
+    ['firedamage'] = true,
+    ['shockdamage'] = true,
+    ['frostdamage'] = true,
+    ['drainattribute'] = true,
+    ['drainhealth'] = true,
+    ['drainspellpoints'] = true,
+    ['drainfatigue'] = true,
+    ['drainskill'] = true,
+    ['damageattribute'] = true,
+    ['damagehealth'] = true,
+    ['damagemagicka'] = true,
+    ['damagefatigue'] = true,
+    ['damageskill'] = true,
+    ['poison'] = true,
+    ['weaknesstofire'] = true,
+    ['weaknesstofrost'] = true,
+    ['weaknesstoshock'] = true,
+    ['weaknesstomagicka'] = true,
+    ['weaknesstocommondisease'] = true,
+    ['weaknesstoblightdisease'] = true,
+    ['weaknesstocorprusdisease'] = true,
+    ['weaknesstopoison'] = true,
+    ['weaknesstonormalweapons'] = true,
+    ['disintegrateweapon'] = true,
+    ['disintegratearmor'] = true,
+    ['paralyze'] = true,
+    ['silence'] = true,
+    ['blind'] = true,
+    ['sound'] = true,
+    ['stuntedmagicka'] = true,
 }
 
 local function isNegativeEffect(e)
@@ -72,6 +443,81 @@ local mortar = nil
 local retort = nil
 local alembic = nil
 local calcinator = nil
+
+local function makePotion(effect, ingr1, ingr2)
+    local p = potion(effect)
+    local m
+    if mortar then
+        m = types.Apparatus.record(mortar).quality
+    else
+        m = 0
+    end
+    local r
+    if retort then
+        r = types.Apparatus.record(retort).quality
+    else
+        r = 0
+    end
+    local a
+    if alembic then
+        a = types.Apparatus.record(alembic).quality
+    else
+        a = 0
+    end
+    local c
+    if calcinator then
+        c = types.Apparatus.record(calcinator).quality
+    else
+        c = 0
+    end
+    m = mortarValue(m)
+    r = retortValue(r)
+    a = alembicValue(a)
+    c = calcinatorValue(c)
+    if p.type == 1 then
+        local count = makePotion1(m, r, a, c, p.difficulty)
+        if count == 0 then
+            core.sendGlobalEvent('A1AlchemyPotion', {
+                player = self.object, id = nil, count = 0, ingr1 = ingr1, ingr2 = ingr2
+            })
+            ui.showMessage('Вам не удалось создать зелье')
+            ambient.playSound('potion fail', nil)
+        else
+            core.sendGlobalEvent('A1AlchemyPotion', {
+                player = self.object, id = p.standard, count = count, ingr1 = ingr1, ingr2 = ingr2
+            })
+            ui.showMessage('Вы создали зелье')
+            ambient.playSound('potion success', nil)
+        end
+    else
+        local quality = makePotion5(m, r, a, c)
+        if quality == 0 then
+            core.sendGlobalEvent('A1AlchemyPotion', {
+                player = self.object, id = nil, count = 0, ingr1 = ingr1, ingr2 = ingr2
+            })
+            ui.showMessage('Вам не удалось создать зелье')
+            ambient.playSound('potion fail', nil)
+        else
+            local id
+            if quality == 1 then
+                id = p.broken
+            elseif quality == 2 then
+                id = p.cheap
+            elseif quality == 3 then
+                id = p.standard
+            elseif quality == 4 then
+                id = p.qualitative
+            else
+                id = p.exclusive
+            end
+            core.sendGlobalEvent('A1AlchemyPotion', {
+                player = self.object, id = id, count = 1, ingr1 = ingr1, ingr2 = ingr2
+            })
+            ui.showMessage('Вы создали зелье')
+            ambient.playSound('potion success', nil)
+        end
+    end
+end
 
 local function findApparatus(apparatus)
     mortar = nil
@@ -427,6 +873,7 @@ local function closeAlchemyMenu()
         end
         alchemyMenu:destroy()
         alchemyMenu = nil
+        alchemyMenuCreateButtonHovered = -1
         mortar = nil
         retort = nil
         alembic = nil
@@ -567,7 +1014,10 @@ local function createAlchemyList(hoverCreateButton)
                                 createButton(
                                     effectInfo,
                                     0,
-                                    function() end,
+                                    function()
+                                        makePotion(e.effect, ingr1, ingr2)
+                                        updateAlchemyMenu(-1)
+                                    end,
                                     function(e)
                                         updateAlchemyMenu(buttonIndexCopy)
                                     end
--- Data Files/Scripts/A1_Alchemy/override.lua
+++ Data Files/Scripts/A1_Alchemy/override.lua
@@ -1,19 +1,23 @@
 local I = require('openmw.interfaces')
 local types = require('openmw.types')
+local world = require('openmw.world')
 
 I.ItemUsage.addHandlerForType(types.Apparatus, function(object, actor)
-    if I.UiMode then
-        I.UiMode.setMode(I.UiMode.getMode()) 
-    end
-    if I.ItemSelection and I.ItemSelection.getSelectedObject then
-        local selectedObj = I.ItemSelection.getSelectedObject()
-        if selectedObj and selectedObj == object then
-            I.ItemSelection.setSelectedObject(nil)
-        end
-    end
     actor:sendEvent("OpenA1Alchemy", { apparatus = object })
     return false
 end)
 
 return {
+    eventHandlers = {
+        A1AlchemyPotion = function(data)
+            if data.id then
+                local o = world.createObject(data.id, data.count)
+                o:moveInto(types.Actor.inventory(data.player))
+            end
+            local ingr1 = data.ingr1
+            ingr1:remove(1)
+            local ingr2 = data.ingr2
+            ingr2:remove(1)
+        end,
+    },
 }
